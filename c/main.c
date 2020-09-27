6c02e83869eb0bb275500cea02a314f38f9e2a60
GiacomoTezza <tezza.giacomo01@gmail.com>
2020-09-27 19:25:15 +0200

    seg fault fixed.. maybe

--- c/main.c
+++ c/main.c
@@ -16,6 +16,7 @@
 #define backgroundred() printf("\033[32;41m")
 #define backgroundgreen() printf("\033[32;42m")
 
+static struct termios term, oterm;
 
 void update(int *board, int *old_gen, int rows, int cols) {
     int i = 0;
@@ -80,38 +81,33 @@ void show(int *board, int rows, int cols) {
 
 int main(int argc, char const *argv[]) {
     struct winsize w;
+    
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     const int rows = w.ws_row;
     const int cols = w.ws_col;
+    // const int rows = 3;
+    // const int cols = 3;
+    int board[rows * cols];
+    int old_gen[rows * cols];
+
+    // int board[9] = {0, 0, 0, 1, 1, 1, 0, 0, 0};
+    // int old_gen[9] = {0, 0, 0, 1, 1, 1, 0, 0, 0};
 
     printf("rows: %i\ncols: %i\n", rows, cols);
 
     // Board init
-    int board[rows][cols];
-    printf("muori qui?");
-    int old_gen[rows][cols];
-    int r;
-    int i, j = 0;
-    while (i < rows) {
-        while (j < cols) {
-            srand(time(NULL));
-            r = rand();
-            printf("random: %i", r);
-            if (r < 0.5) {
-                board[i][j] = 0;
-            } else {
-                board[i][j] = 1;
-            }
-            j++;
+    srand(time(NULL));
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            board[i * cols + j] = rand() % 2;
         }
-        i++;
     }
-    printf("initialization completed");
+    printf("init completed\n");
 
-    int *board_p = &board;
-    int *old_gen_p = &old_gen;
+    int (*board_p)[] = &board;
+    int (*old_gen_p)[] = &old_gen;
 
-    // Game Loop
+    // // Game Loop
     int loops = 100;
     while(loops > 0) {
         show(*board_p, rows, cols);
